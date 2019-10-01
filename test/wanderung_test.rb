1e2ad0d1be649112420bc1d58ccdf54d313e9a62
Diego Camargo <dicamargov@gmail.com>
2019-10-01 10:57:12 -0500

    Remove failing test

--- test/wanderung_test.rb
+++ test/wanderung_test.rb
@@ -6,8 +6,4 @@ class WanderungTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Wanderung::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
